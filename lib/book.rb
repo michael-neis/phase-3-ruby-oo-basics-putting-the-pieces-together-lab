2dfead5150cc3dbeca9fffb1196bd753da570447
Michael Neis <michael.neis12@gmail.com>
2021-11-28 19:22:00 -0600

    commit tree 12fcb78dbdb30efe61974d9d1c6fcee69cf6ee75

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
 
 
+class Book
+
+    attr_accessor :title, :genre, :page_count, :author
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
